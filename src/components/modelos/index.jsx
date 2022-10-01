2960ccf918a9b640d068b4a305500abf5f48382e
Bruno Gouveia <78083006+brunovcg@users.noreply.github.com>
2022-10-01 15:07:27 -0300

    Bruno refactor add hook to models

--- src/components/modelos/index.jsx
+++ src/components/modelos/index.jsx
@@ -1,16 +1,14 @@
 import { Container } from "./styled";
 import { modelsOverflow, modelsOverflowFun } from "../../DB";
 import { useMemo, useState } from "react";
-import ModelsTypes from "./modelsTypes";
-import { useNavigate } from "react-router-dom";
-import useOptions from "./useOptions";
+import ModelsTypes from "./templates/modelsTypes";
+import useOptions from "./hooks/useOptions";
+import ModelsRender from "./templates/modelsRender";
 
 function BarraDeModelos() {
   const [filterCF, setFilterCF] = useState("todos");
 
   const [filterType, setFilterType] = useState("cf");
-
-  const navigate = useNavigate();
 
   const reducedQuads = useMemo(
     () =>
@@ -33,56 +31,21 @@ function BarraDeModelos() {
     setFilterCF
   );
 
-  const handleFilter = (item) => {
-    if (filterCF === "todos") {
-      return item.type === filterType;
-    }
-    return item.type === filterType && filterCF === item.category;
-  };
-
-  const handleClick = (item, model) => {
-    navigate(`/modelos/${model}/${item}`);
-  };
-
   return (
     <Container>
       <div id="modelos" className="boxBarraLaranja">
         <div className="barraDeModelos">
           <span className="barraLaranja" />
-
           <ModelsTypes modelOptions={modelOptions} />
         </div>
       </div>
-      <ul className="listaDeModelos">
-        {typeOptions
-          .filter((item) => item.type === filterType)
-          .map((item) => (
-            <li key={item.id} className={item.className} onClick={item.onClick}>
-              {item.title}
-            </li>
-          ))}
-      </ul>
-      <div className="modelsCards">
-        <div className="boxQuads">
-          {reducedQuads.filter(handleFilter).map((item, index) => {
-            return (
-              <div className="quadsCards" key={index}>
-                <img
-                  className="imageModels"
-                  src={item.img}
-                  key={item.id}
-                  alt="First slide"
-                  onClick={() => handleClick(item.model, item.description)}
-                />
-                <div className="modelsNames">
-                  <p>{item.description}</p>
-                  <p>{item.model}</p>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </div>
+
+      <ModelsRender
+        reducedQuads={reducedQuads}
+        typeOptions={typeOptions}
+        filterCF={filterCF}
+        filterType={filterType}
+      />
     </Container>
   );
 }
---
+++ src/components/modelos/templates/modelsRender.jsx
@@ -0,0 +1,53 @@
+import { useNavigate } from "react-router-dom";
+
+function ModelsRender({ typeOptions, filterType, reducedQuads, filterCF }) {
+  const navigate = useNavigate();
+
+  const handleClick = (item, model) => {
+    navigate(`/modelos/${model}/${item}`);
+  };
+
+  const handleFilter = (item) => {
+    if (filterCF === "todos") {
+      return item.type === filterType;
+    }
+    return item.type === filterType && filterCF === item.category;
+  };
+  
+  return (
+    <>
+      <ul className="listaDeModelos">
+        {typeOptions
+          .filter((item) => item.type === filterType)
+          .map((item) => (
+            <li key={item.id} className={item.className} onClick={item.onClick}>
+              {item.title}
+            </li>
+          ))}
+      </ul>
+      <div className="modelsCards">
+        <div className="boxQuads">
+          {reducedQuads.filter(handleFilter).map((item, index) => {
+            return (
+              <div className="quadsCards" key={index}>
+                <img
+                  className="imageModels"
+                  src={item.img}
+                  key={item.id}
+                  alt="First slide"
+                  onClick={() => handleClick(item.model, item.description)}
+                />
+                <div className="modelsNames">
+                  <p>{item.description}</p>
+                  <p>{item.model}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ModelsRender;
